4e36330f2e2494d9ccf2c977f975ebb18c85bb07
yana-ochen <yana-ochen@TeamCity>
2025-09-02 19:16:36 +0000

    Versioned settings configuration updated (TeamCity change in 'Java Sepa' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,69 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2025.07"
+
+project {
+
+    vcsRoot(HttpsGithubComJavaSepaXmlJavaSepaXmlRefsHeadsMaster)
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(HttpsGithubComJavaSepaXmlJavaSepaXmlRefsHeadsMaster)
+    }
+
+    steps {
+        maven {
+            id = "Maven2"
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
+
+object HttpsGithubComJavaSepaXmlJavaSepaXmlRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/java-sepa-xml/java-sepa-xml#refs/heads/master"
+    url = "https://github.com/java-sepa-xml/java-sepa-xml"
+    branch = "refs/heads/master"
+    branchSpec = "refs/heads/*"
+})
